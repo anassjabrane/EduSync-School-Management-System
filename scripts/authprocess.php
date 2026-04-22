c67ee1fe12be1de45e6d09fc285c59eaef43aa94
Anass Jabrane <jabraneanass12@gmail.com>
2026-04-22 15:30:46 +0100

    Récupération des données avec la méthode

--- scripts/authprocess.php
+++ scripts/authprocess.php
@@ -1,2 +1,8 @@
 <?php 
 
+$lastname = htmlspecialchars($_POST['lastname']);
+$firstname =htmlspecialchars($_POST['firstname']);
+$email =htmlspecialchars($_POST['email']);
+$password =htmlspecialchars($_POST['password']);
+
+
