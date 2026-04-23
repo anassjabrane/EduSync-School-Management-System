b5535340119078684823d392ec25406cbb1df0b8
Anass Jabrane <jabraneanass12@gmail.com>
2026-04-23 10:26:22 +0100

    Vérification login et gestion erreur

--- scripts/authprocess.php
+++ scripts/authprocess.php
@@ -26,5 +26,27 @@ $email =htmlspecialchars($email);
 echo "Données valides ✔️";
 
 
+$email=($_POST['email']);
+$password =($_POST['password']);
+
+
+if(empty($email)||empty($password)){
+    header("location:login.php");
+    exit;
+}
+
+if($email=== "anassjabrane12@gmail.com" && $password === "123456789"){
+   
+header("location:dashbord.php");
+exit ;
+}
+
+else{
+    header("location :login.php?error=1");
+    exit ;
+}
+
+
+
 
 
