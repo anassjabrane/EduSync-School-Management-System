f1f83179170605f38ae7312e7a34d153ed97ea44
Anass Jabrane <jabraneanass12@gmail.com>
2026-04-24 15:27:53 +0100

    envoyer register vers dashboard

--- scripts/authprocess.php
+++ scripts/authprocess.php
@@ -24,33 +24,17 @@ $email =htmlspecialchars($email);
 
 
 
-echo "Données valides ✔️";
+session_start();
+
+$_SESSION["user"] = [
+    "nom" => $firstname,
+    "role" => "student"
+];
+
+header("Location: ../public/dashbord.php");
+exit;
 
 
-///////////////////////////////////////////////
-
-// session_start();
-// if (empty($email) || empty($password)) {
-//     header("Location: ../public/login.php?error=empty");
-//     exit;
-// }
-
-// if ($email === "test@gmail.com" && $password === "123456") {
-
-//     $_SESSION["user"] = [
-//         "nom" => "test",
-//         "role" => "student"
-//     ];
-
-//     header("Location: ../public/dashboard.php");
-//     exit;
-
-// } else {
-
-
-//     header("Location: ../public/login.php?error=1");
-//     exit;
-// }
 
 
 
